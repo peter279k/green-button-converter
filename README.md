bc33bd01e3aa5194f566a1f9be0acc6785b475a3
peter279k <peter279k@gmail.com>
2019-07-19 13:26:48 +0800

    Add README usage, enhance DataWriter output

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
 # Green Button Data Converter
+
+## Usage
+To generate a Green button data format, you can use following code snippets:
+
+```JS
+var DataWriter = require('green-button-data-converter').DataWriter
+let options = {
+  'interval_duration': 86400,
+  'time_period_duration': 3600,
+  'start_date': '2019-07-18 00:00:00',
+  'interval_readings': [
+    {date: '2019-07-18 17:00:00', mwh: 912},
+    {date: '2019-07-18 18:00:00', mwh: 900},
+  ],
+}
+
+let expectedXmlHead = '<?xml version="1.0" encoding="utf-8"?>'
+let expectedFeed = '<feed xmlns="http://www.w3.org/2005/Atom">'
+let dataWriter = new DataWriter(options)
+
+assert.strictEqual(dataWriter.output().includes(expectedXmlHead), true)
+assert.strictEqual(dataWriter.output().includes(expectedFeed), true)
+```
+
+It will have following result:
+
+```XML
+<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+    <entry>
+        <id>urn:uuid:5F418672-E558-4DC0-95A1-666D066D99AE</id>
+        <title/>
+        <content>
+            <UsagePoint xmlns="http://naesb.org/espi">
+                <ServiceCategory>
+                    <kind>0</kind>
+                </ServiceCategory>
+            </UsagePoint>
+        </content>
+        <content>
+            <ReadingType xmlns="http://naesb.org/espi">
+                <accumulationBehaviour>4</accumulationBehaviour>
+                <commodity>1</commodity>
+                <dataQualifier>12</dataQualifier>
+                <flowDirection>1</flowDirection>
+                <intervalLength>3600</intervalLength>
+                <kind>12</kind>
+                <phase>769</phase>
+                <powerOfTenMultiplier>0</powerOfTenMultiplier>
+                <timeAttribute>0</timeAttribute>
+                <uom>72</uom>
+            </ReadingType>
+        </content>
+        <content>
+            <IntervalBLock xmlns="http://naesb.org/espi">
+                <interval>
+                    <duration>86400</duration>
+                    <start>1563440400</start>
+                </interval>
+                <IntervalReading>
+                    <timePeriod>
+                        <duration>3600</duration>
+                        <start>2019-07-18 17:00:00</start>
+                    </timePeriod>
+                    <value>912</value>
+                </IntervalReading>
+            </IntervalBLock>
+        </content>
+        <published>2019-07-19T13:25:14+08:00</published>
+        <updated>2019-07-19T13:25:14+08:00</updated>
+    </entry>
+</feed>
+```
--- test/data-writer.js
+++ test/data-writer.js
@@ -1,21 +1,22 @@
 'use strict'
 
 var assert = require('assert')
-var greenButtonData = require('../lib/index')
+var DataWriter = require('../lib/index').DataWriter
+var options = {
+  'interval_duration': 86400,
+  'time_period_duration': 3600,
+  'start_date': '2019-07-18 17:00:00',
+  'interval_readings': [
+    {date: '2019-07-18 17:00:00', mwh: 912},
+  ],
+  'power_of_ten_multiplier': 0,
+  'uom': 72,
+}
 
 describe('data-writer', function() {
     describe('#validateOptions()', function() {
       it('can validate options without error', function(done) {
-        let options = {
-            'interval_duration': 86400,
-            'time_period_duration': 3600,
-            'start_date': '2019-07-18 17:00:00',
-            'interval_readings': [
-              {date: '2019-07-18 17:00:00', mwh: 912},
-            ],
-        }
-
-        let dataWriter = new greenButtonData.DataWriter(options)
+        let dataWriter = new DataWriter(options)
         assert.strictEqual(dataWriter.validateOptions(), true)
 
         done()
@@ -25,8 +26,8 @@ describe('data-writer', function() {
 
 describe('data-writer', function() {
   describe('#getPublishedDate', function() {
-    it('can get curent published date with ISO-8661-1988 format', function (done) {
-      assert.strictEqual(typeof greenButtonData.DataWriter.getPublishedDate() === 'string', true)
+    it('can get current published date with ISO-8661-1988 format', function (done) {
+      assert.strictEqual(typeof DataWriter.getPublishedDate() === 'string', true)
 
       done()
     })
@@ -35,17 +36,8 @@ describe('data-writer', function() {
 
 describe('data-writer', function() {
   describe('#getStartTimestamp', function() {
-    it('can get curent start timestamp', function (done) {
-      let options = {
-        'interval_duration': 86400,
-        'time_period_duration': 3600,
-        'start_date': '2019-07-18 17:00:00',
-        'interval_readings': [
-          {date: '2019-07-18 17:00:00', mwh: 912},
-        ],
-      }
-
-      let dataWriter = new greenButtonData.DataWriter(options)
+    it('can get current start timestamp', function (done) {
+      let dataWriter = new DataWriter(options)
       assert.strictEqual(typeof dataWriter.getStartTimestamp() === 'number', true)
 
       done()
@@ -57,7 +49,7 @@ describe('data-writer', function() {
   describe('#getId', function() {
     it('can get UUIDv4 with entry id', function(done) {
       let test = new RegExp(/[\w+]{8}-[\w+]{4}-[\w+]{4}-[\w+]{4}-[\w+]{12}/)
-      assert.strictEqual(test.test(greenButtonData.DataWriter.getId()), true)
+      assert.strictEqual(test.test(DataWriter.getId()), true)
 
       done()
     })
@@ -67,22 +59,15 @@ describe('data-writer', function() {
 describe('data-writer', function() {
   describe('#output', function() {
     it('can get output Green button data with minified Atom XML', function(done) {
-      let options = {
-        'interval_duration': 86400,
-        'time_period_duration': 3600,
-        'start_date': '2019-07-18 00:00:00',
-        'interval_readings': [
-          {date: '2019-07-18 17:00:00', mwh: 912},
-          {date: '2019-07-18 18:00:00', mwh: 900},
-        ],
-      }
-
       let expectedXmlHead = '<?xml version="1.0" encoding="utf-8"?>'
       let expectedFeed = '<feed xmlns="http://www.w3.org/2005/Atom">'
-      let dataWriter = new greenButtonData.DataWriter(options)
+      let dataWriter = new DataWriter(options)
+      let result = dataWriter.output()
 
-      assert.strictEqual(dataWriter.output().includes(expectedXmlHead), true)
-      assert.strictEqual(dataWriter.output().includes(expectedFeed), true)
+      console.log(result)
+
+      assert.strictEqual(result.includes(expectedXmlHead), true)
+      assert.strictEqual(result.includes(expectedFeed), true)
 
       done()
     })
